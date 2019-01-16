94a55f1f8cf54aba5b9173ec9f22620dff63f2dc
Gabriel Delgado <gabewood@Gabriels-MacBook-Air.local>
2019-01-15 21:17:47 -0700

    updated filepath

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l',
     'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-// Variables for tracking wins and losses
+// Variables for tracking wins,losses, and ties
 var wins = 0;
 var losses = 0;
 var guesses = 10;
